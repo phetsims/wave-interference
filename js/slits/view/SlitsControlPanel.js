ecb2fb28b1c9452437bf0dcbdb4cf351445caea9
samreid <reids@colorado.edu>
2018-03-17 17:44:57 -0600

    Fixed lint issue

--- js/slits/view/SlitsControlPanel.js
+++ js/slits/view/SlitsControlPanel.js
@@ -15,7 +15,6 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var NumberControl = require( 'SCENERY_PHET/NumberControl' );
-  var Property = require( 'AXON/Property' );
   var Range = require( 'DOT/Range' );
   var waveInterference = require( 'WAVE_INTERFERENCE/waveInterference' );
   var WaveInterferencePanel = require( 'WAVE_INTERFERENCE/common/view/WaveInterferencePanel' );
