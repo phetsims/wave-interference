eb001d19bf593caceaab82d80560b56eee129390
samreid <reids@colorado.edu>
2018-04-04 14:06:48 -0600

    Added stroke around dashed line

--- js/common/view/DottedLineNode.js
+++ js/common/view/DottedLineNode.js
@@ -10,24 +10,32 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var HBox = require( 'SCENERY/nodes/HBox' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Line = require( 'SCENERY/nodes/Line' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var waveInterference = require( 'WAVE_INTERFERENCE/waveInterference' );
   var WaveInterferenceConstants = require( 'WAVE_INTERFERENCE/common/WaveInterferenceConstants' );
+
+  // constants
+  var SPACING = 16.2;
+  var DASH_LENGTH = 16.2;
 
   /**
    * @param {Object} [options]
    * @constructor
    */
   function DottedLineNode( options ) {
-    Line.call( this, 0, 0, WaveInterferenceConstants.WAVE_AREA_WIDTH, 0, _.extend( {
-      stroke: 'white',
-      lineWidth: 3,
-      lineDash: [ 16.2, 16.2 ] // Adjusted so the dash begins and ends on the lattice (phase)
+
+    // Render as an HBox of Rectangle so that each dash can have its own stroke
+    HBox.call( this, _.extend( {
+      spacing: SPACING,
+      children: _.times( WaveInterferenceConstants.WAVE_AREA_WIDTH / ( DASH_LENGTH + SPACING ), function() {
+        return new Rectangle( 0, 0, DASH_LENGTH, 4, { fill: 'white', stroke: 'black', lineWidth: 1 } );
+      } )
     }, options ) );
   }
 
   waveInterference.register( 'DottedLineNode', DottedLineNode );
 
-  return inherit( Line, DottedLineNode );
+  return inherit( HBox, DottedLineNode );
 } );
