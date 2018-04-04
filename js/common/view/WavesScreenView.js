1db6aa3287f929983d898632ed2c6254697f9ef4
samreid <reids@colorado.edu>
2018-04-04 11:32:50 -0600

    Factored out laser pointer options

--- js/common/view/WavesScreenView.js
+++ js/common/view/WavesScreenView.js
@@ -333,22 +333,15 @@ define( function( require ) {
     } );
     this.addChild( perspective3DNode );
 
-    var laserPointerNode1 = new LaserPointerNode( model.button1PressedProperty, {
+    var LASER_OPTIONS = {
       bodySize: new Dimension2( 80, 40 ),
       nozzleSize: new Dimension2( 10, 28 ),
       buttonRadius: 18,
       hasGlass: true,
       rightCenter: this.waveAreaNode.leftCenter.plusXY( 20, 0 )
-    } );
-
-    // TODO: factor out options
-    var laserPointerNode2 = new LaserPointerNode( model.button2PressedProperty, {
-      bodySize: new Dimension2( 80, 40 ),
-      nozzleSize: new Dimension2( 10, 28 ),
-      buttonRadius: 18,
-      hasGlass: true,
-      rightCenter: this.waveAreaNode.leftCenter.plusXY( 20, 100 )
-    } );
+    };
+    var laserPointerNode1 = new LaserPointerNode( model.button1PressedProperty, LASER_OPTIONS );
+    var laserPointerNode2 = new LaserPointerNode( model.button2PressedProperty, LASER_OPTIONS );
 
     var updateEnabled = function() {
       if ( model.inputTypeProperty.value === InputTypeEnum.PULSE ) {
