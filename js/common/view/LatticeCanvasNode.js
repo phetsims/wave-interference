1c69085eb17f92fbe20baf1921a7da0d1781554b
samreid <reids@colorado.edu>
2018-05-16 06:35:14 -0600

    lint

--- js/common/view/LatticeCanvasNode.js
+++ js/common/view/LatticeCanvasNode.js
@@ -9,7 +9,6 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var Bounds2 = require( 'DOT/Bounds2' );
   var CanvasNode = require( 'SCENERY/nodes/CanvasNode' );
   var Color = require( 'SCENERY/util/Color' );
   var inherit = require( 'PHET_CORE/inherit' );
