3a774b80eec57b84ddeda199db1b2f1239960344
samreid <reids@colorado.edu>
2018-07-08 11:19:49 -0600

    Fixed lint issue, see https://github.com/phetsims/wave-interference/issues/77

--- js/waves/model/WavesScreenModel.js
+++ js/waves/model/WavesScreenModel.js
@@ -14,7 +14,6 @@ define( function( require ) {
   // modules
   var BooleanProperty = require( 'AXON/BooleanProperty' );
   var DerivedProperty = require( 'AXON/DerivedProperty' );
-  var DynamicProperty = require( 'AXON/DynamicProperty' );
   var Emitter = require( 'AXON/Emitter' );
   var EventTimer = require( 'PHET_CORE/EventTimer' );
   var IncomingWaveType = require( 'WAVE_INTERFERENCE/common/model/IncomingWaveType' );
