a924c0c395b7a5baad33eedc1d474d6a77f92463
samreid <reids@colorado.edu>
2018-08-13 22:50:33 -0600

    Moved probe logic to subclass, see https://github.com/phetsims/wave-interference/issues/24

--- js/common/view/WaveDetectorToolNode.js
+++ js/common/view/WaveDetectorToolNode.js
@@ -9,26 +9,10 @@ define( require => {
   'use strict';
 
   // modules
-  const Color = require( 'SCENERY/util/Color' );
-  const DerivedProperty = require( 'AXON/DerivedProperty' );
   const DragListener = require( 'SCENERY/listeners/DragListener' );
+  const Emitter = require( 'AXON/Emitter' );
   const Node = require( 'SCENERY/nodes/Node' );
-  const NodeProperty = require( 'SCENERY/util/NodeProperty' );
-  const Property = require( 'AXON/Property' );
-  const Vector2 = require( 'DOT/Vector2' );
-  const WaveDetectorToolProbeNode = require( 'WAVE_INTERFERENCE/common/view/WaveDetectorToolProbeNode' );
   const waveInterference = require( 'WAVE_INTERFERENCE/waveInterference' );
-  const WireNode = require( 'SCENERY_PHET/WireNode' );
-
-  // constants
-  const SERIES_1_COLOR = '#5c5d5f'; // same as in Bending Light
-  const SERIES_2_COLOR = '#ccced0'; // same as in Bending Light
-  const WIRE_2_COLOR = new Color( SERIES_2_COLOR ).darkerColor( 0.7 );
-
-  // For the wires
-  const NORMAL_DISTANCE = 25;
-  const PROBE_ATTACHMENT_POINT = 'centerBottom';
-  const WIRE_LINE_WIDTH = 3;
 
   class WaveDetectorToolNode extends Node {
 
@@ -66,39 +50,8 @@ define( require => {
       this.backgroundNode.addInputListener( this.backgroundDragListener );
       this.addChild( this.backgroundNode );
 
-      const bodyNormalProperty = new Property( new Vector2( NORMAL_DISTANCE, 0 ) );
-      const sensorNormalProperty = new Property( new Vector2( 0, NORMAL_DISTANCE ) );
-
-      const getPointAbove = amount => position => position.plusXY( 0, -amount );
-
-      // These do not need to be disposed because there is no connection to the "outside world"
-      const rightBottomProperty = new NodeProperty( this.backgroundNode, 'bounds', 'rightBottom' );
-      const aboveBottomRight1Property = new DerivedProperty( [ rightBottomProperty ], getPointAbove( 20 ) );
-      const aboveBottomRight2Property = new DerivedProperty( [ rightBottomProperty ], getPointAbove( 10 ) );
-
-      // @private {Node}
-      this.probe1Node = new WaveDetectorToolProbeNode( { color: SERIES_1_COLOR } );
-
-      // @private
-      this.addChild( new WireNode( aboveBottomRight1Property, bodyNormalProperty,
-        new NodeProperty( this.probe1Node, 'bounds', PROBE_ATTACHMENT_POINT ), sensorNormalProperty, {
-          lineWidth: WIRE_LINE_WIDTH,
-          stroke: SERIES_1_COLOR
-        }
-      ) );
-      this.addChild( this.probe1Node );
-
-      // @private {Node}
-      this.probe2Node = new WaveDetectorToolProbeNode( { color: SERIES_2_COLOR } );
-
-      // @private
-      this.addChild( new WireNode( aboveBottomRight2Property, bodyNormalProperty,
-        new NodeProperty( this.probe2Node, 'bounds', PROBE_ATTACHMENT_POINT ), sensorNormalProperty, {
-          lineWidth: WIRE_LINE_WIDTH,
-          stroke: WIRE_2_COLOR
-        }
-      ) );
-      this.addChild( this.probe2Node );
+      // @public (listen-only)
+      this.alignProbesEmitter = new Emitter();
 
       this.alignProbes();
 
@@ -110,15 +63,7 @@ define( require => {
      * @public
      */
     alignProbes() {
-      this.probe1Node.mutate( {
-        left: this.backgroundNode.right + 5,
-        top: this.backgroundNode.top + 10
-      } );
-
-      this.probe2Node.mutate( {
-        left: this.backgroundNode.right + 36,
-        top: this.backgroundNode.top + 54
-      } );
+      this.alignProbesEmitter.emit();
     }
 
     /**
--- js/common/view/WaveInterferenceWaveDetectorToolNode.js
+++ js/common/view/WaveInterferenceWaveDetectorToolNode.js
@@ -14,6 +14,24 @@ define( require => {
   const WaveDetectorToolContentNode = require( 'WAVE_INTERFERENCE/common/view/WaveDetectorToolContentNode' );
   const WaveDetectorToolNode = require( 'WAVE_INTERFERENCE/common/view/WaveDetectorToolNode' );
   const waveInterference = require( 'WAVE_INTERFERENCE/waveInterference' );
+  const DerivedProperty = require( 'AXON/DerivedProperty' );
+  const NodeProperty = require( 'SCENERY/util/NodeProperty' );
+  const Property = require( 'AXON/Property' );
+  const Vector2 = require( 'DOT/Vector2' );
+  const WaveDetectorToolProbeNode = require( 'WAVE_INTERFERENCE/common/view/WaveDetectorToolProbeNode' );
+  const WireNode = require( 'SCENERY_PHET/WireNode' );
+  const Color = require( 'SCENERY/util/Color' );
+
+  // constants
+  const SERIES_1_COLOR = '#5c5d5f'; // same as in Bending Light
+  const SERIES_2_COLOR = '#ccced0'; // same as in Bending Light
+  const WIRE_1_COLOR = SERIES_1_COLOR;
+  const WIRE_2_COLOR = new Color( SERIES_2_COLOR ).darkerColor( 0.7 );
+
+  // For the wires
+  const NORMAL_DISTANCE = 25;
+  const PROBE_ATTACHMENT_POINT = 'centerBottom';
+  const WIRE_LINE_WIDTH = 3;
 
   class WaveInterferenceWaveDetectorToolNode extends WaveDetectorToolNode {
 
@@ -23,12 +41,43 @@ define( require => {
      * @param {Object} [options]
      */
     constructor( model, view, options ) {
-      super( new ShadedRectangle( new Bounds2( 0, 0, 181.5, 145.2 ), {
+      const backgroundNode = new ShadedRectangle( new Bounds2( 0, 0, 181.5, 145.2 ), {
         cursor: 'pointer'
-      } ), options );
+      } );
+      super( backgroundNode, options );
+
+      // These do not need to be disposed because there is no connection to the "outside world"
+      const rightBottomProperty = new NodeProperty( backgroundNode, 'bounds', 'rightBottom' );
+
+      const createProbeAndWire = ( color, wireColor, connectionProperty ) => {
+        const probeNode = new WaveDetectorToolProbeNode( { color } );
+        this.addChild( new WireNode( connectionProperty, new Property( new Vector2( NORMAL_DISTANCE, 0 ) ),
+          new NodeProperty( probeNode, 'bounds', PROBE_ATTACHMENT_POINT ), new Property( new Vector2( 0, NORMAL_DISTANCE ) ), {
+            lineWidth: WIRE_LINE_WIDTH,
+            stroke: wireColor
+          }
+        ) );
+        this.addChild( probeNode );
+        return probeNode;
+      };
+
+      const getPointAbove = amount => position => position.plusXY( 0, -amount );
+
+      // @private {Node}
+      this.probe1Node = createProbeAndWire( SERIES_1_COLOR, WIRE_1_COLOR, new DerivedProperty( [ rightBottomProperty ], getPointAbove( 20 ) ) );
+
+      // @private {Node}
+      this.probe2Node = createProbeAndWire( SERIES_2_COLOR, WIRE_2_COLOR, new DerivedProperty( [ rightBottomProperty ], getPointAbove( 10 ) ) );
+
+      const alignProbes = () => {
+        this.probe1Node.mutate( { left: backgroundNode.right + 5, top: backgroundNode.top + 10 } );
+        this.probe2Node.mutate( { left: backgroundNode.right + 36, top: backgroundNode.top + 54 } );
+      };
+      this.alignProbesEmitter.addListener( alignProbes );
+      alignProbes();
 
       // TODO: embed onto the background as a child
-      const waveDetectorToolContentNode = new WaveDetectorToolContentNode( model, view, this.backgroundNode, this.probe1Node, this.probe2Node, options );
+      const waveDetectorToolContentNode = new WaveDetectorToolContentNode( model, view, backgroundNode, this.probe1Node, this.probe2Node, options );
       this.addChild( waveDetectorToolContentNode );
     }
   }
