eac235fa19d72994d4e08f424053a55b462e2c32
samreid <reids@colorado.edu>
2018-08-13 22:56:42 -0600

    Factored out more probe-specific logic, see https://github.com/phetsims/wave-interference/issues/24

--- js/common/view/WaveInterferenceWaveDetectorToolNode.js
+++ js/common/view/WaveInterferenceWaveDetectorToolNode.js
@@ -49,8 +49,17 @@ define( require => {
       // These do not need to be disposed because there is no connection to the "outside world"
       const rightBottomProperty = new NodeProperty( backgroundNode, 'bounds', 'rightBottom' );
 
-      const createProbeAndWire = ( color, wireColor, connectionProperty ) => {
+      /**
+       * @param {Color} color
+       * @param {Color} wireColor
+       * @param {number} dx
+       * @param {number} dy
+       * @param {Property.<Vector2>} connectionProperty
+       */
+      const createProbeAndWire = ( color, wireColor, dx, dy, connectionProperty ) => {
         const probeNode = new WaveDetectorToolProbeNode( { color } );
+
+        // Add the wire behind the probe.
         this.addChild( new WireNode( connectionProperty, new Property( new Vector2( NORMAL_DISTANCE, 0 ) ),
           new NodeProperty( probeNode, 'bounds', PROBE_ATTACHMENT_POINT ), new Property( new Vector2( 0, NORMAL_DISTANCE ) ), {
             lineWidth: WIRE_LINE_WIDTH,
@@ -58,23 +67,24 @@ define( require => {
           }
         ) );
         this.addChild( probeNode );
+
+        // Standard location in toolbox and when dragging out of toolbox.
+        const alignProbes = () => probeNode.mutate( { left: backgroundNode.right + dx, top: backgroundNode.top + dy } );
+        this.alignProbesEmitter.addListener( alignProbes );
+        alignProbes();
+
         return probeNode;
       };
 
-      const getPointAbove = amount => position => position.plusXY( 0, -amount );
+      // @private {Node}
+      this.probe1Node = createProbeAndWire( SERIES_1_COLOR, WIRE_1_COLOR, 5, 10,
+        new DerivedProperty( [ rightBottomProperty ], position => position.plusXY( 0, -20 ) )
+      );
 
       // @private {Node}
-      this.probe1Node = createProbeAndWire( SERIES_1_COLOR, WIRE_1_COLOR, new DerivedProperty( [ rightBottomProperty ], getPointAbove( 20 ) ) );
-
-      // @private {Node}
-      this.probe2Node = createProbeAndWire( SERIES_2_COLOR, WIRE_2_COLOR, new DerivedProperty( [ rightBottomProperty ], getPointAbove( 10 ) ) );
-
-      const alignProbes = () => {
-        this.probe1Node.mutate( { left: backgroundNode.right + 5, top: backgroundNode.top + 10 } );
-        this.probe2Node.mutate( { left: backgroundNode.right + 36, top: backgroundNode.top + 54 } );
-      };
-      this.alignProbesEmitter.addListener( alignProbes );
-      alignProbes();
+      this.probe2Node = createProbeAndWire( SERIES_2_COLOR, WIRE_2_COLOR, 36, 54,
+        new DerivedProperty( [ rightBottomProperty ], position => position.plusXY( 0, -10 ) )
+      );
 
       // TODO: embed onto the background as a child
       const waveDetectorToolContentNode = new WaveDetectorToolContentNode( model, view, backgroundNode, this.probe1Node, this.probe2Node, options );
