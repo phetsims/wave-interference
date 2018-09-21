65ceb53848f16336728d60d67bb28ce9024a6ed8
samreid <reids@colorado.edu>
2018-09-21 13:51:54 -0600

    Removed slits radio button for now, see https://github.com/phetsims/wave-interference/issues/148

--- js/diffraction/view/DiffractionScreenView.js
+++ js/diffraction/view/DiffractionScreenView.js
@@ -96,12 +96,6 @@ define( require => {
       }, {
         value: 'circle',
         node: new Circle( 10, { fill: 'black' } )
-      }, {
-        value: 'slits',
-        node: new HBox( {
-          spacing: 2,
-          children: _.range( 1, 8 ).map( r => new Rectangle( 0, 0, 2, 20, { fill: 'black' } ) )
-        } )
       } ];
       const radioButtonGroup = new RadioButtonGroup( this.sceneProperty, toggleButtonsContent, {
         left: 10,
