1c14122a5384ab3f1c9c4be7cc9f43663e95930e
samreid <reids@colorado.edu>
2018-11-12 08:56:03 -0700

    Remove completed TODO

--- js/common/model/WaveTemporalType.js
+++ js/common/model/WaveTemporalType.js
@@ -1,7 +1,7 @@
 // Copyright 2018, University of Colorado Boulder
 
 /**
- * Determines whether the incoming wave is continuous or pulse.  TODO: rename WaveTemporalType.js
+ * Determines whether the incoming wave is continuous or pulse.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
