b8425a486981d383ecc8d99877ac3cd0c3e2df0e
samreid <reids@colorado.edu>
2018-11-26 21:37:15 -0700

    Revise docs, see https://github.com/phetsims/wave-interference/issues/191

--- js/common/WaveInterferenceQueryParameters.js
+++ js/common/WaveInterferenceQueryParameters.js
@@ -17,9 +17,8 @@ define( require => {
     // be shared with teachers and should not be changed lightly.
     theory: { type: 'flag' },
 
-    // This is a temporary query parameter that adds the in-development "Diffraction" screen.
-    // Once the Diffraction screen is included in a published version, this query parameter
-    // will be deleted.
+    // This is a temporary query parameter that adds the in-development "Diffraction" screen. Once the Diffraction
+    // screen is included in a published version, this query parameter will be deleted.
     includeDiffractionScreen: { type: 'flag' }
   } );
 
