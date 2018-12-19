f4ac7cb978749e489d8d6b5819d6655b53c0a0ae
samreid <reids@colorado.edu>
2018-12-18 21:52:04 -0700

    Add docs, see https://github.com/phetsims/wave-interference/issues/269

--- js/common/WaveInterferenceUtils.js
+++ js/common/WaveInterferenceUtils.js
@@ -63,7 +63,7 @@ define( require => {
       //REVIEW explain the magic numbers here
       //REVIEW* Done, please review.
       // Typical values for the propagating wave can be between -5 and 5 (though values can exceed this range very close to the
-      // oscillating cell.  We choose to map a value of 0 to the ceter of the wave area, and the max (5) to the desired
+      // oscillating cell.  We choose to map a value of 0 to the center of the wave area, and the max (5) to the desired
       // distance amplitude.
       return Util.linear( 0, 5, waveAreaBounds.centerY, waveAreaBounds.centerY - 80, waveValue );
     }
