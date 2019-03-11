47f1d6b0fac3cf8ccaef61c46b62c648a29a29de
samreid <reids@colorado.edu>
2019-03-11 12:28:45 -0600

    Eliminate overhead in FFT postprocessing

--- js/diffraction/model/DiffractionModel.js
+++ js/diffraction/model/DiffractionModel.js
@@ -10,7 +10,6 @@ define( require => {
 
   // modules
   const BooleanProperty = require( 'AXON/BooleanProperty' );
-  const Complex = require( 'DOT/Complex' );
   const EllipseScene = require( 'WAVE_INTERFERENCE/diffraction/model/EllipseScene' );
   const Enumeration = require( 'PHET_CORE/Enumeration' );
   const Matrix = require( 'DOT/Matrix' );
@@ -27,10 +26,12 @@ define( require => {
   const DEFAULT_WAVELENGTH = ( VisibleColor.MIN_WAVELENGTH + VisibleColor.MAX_WAVELENGTH ) / 2;
   const MATRIX_DIMENSION = WaveInterferenceConstants.DIFFRACTION_MATRIX_DIMENSION;
 
-  // preallocated once to avoid generating garbage
-  const SCRATCH_SHIFTED = new Array( MATRIX_DIMENSION * MATRIX_DIMENSION );
+  // preallocated to avoid generating garbage
+  const SHIFTED_MAGNITUDES = new Array( MATRIX_DIMENSION * MATRIX_DIMENSION );
   const im = new Array( MATRIX_DIMENSION * MATRIX_DIMENSION );
   const re = new Array( MATRIX_DIMENSION * MATRIX_DIMENSION );
+
+  // initialization for 3rd party library
   FFT.init( MATRIX_DIMENSION );
 
   class DiffractionModel {
@@ -116,22 +117,6 @@ define( require => {
    */
   DiffractionModel.ApertureType = new Enumeration( [ 'CIRCLE', 'RECTANGLE', 'SLITS' ] );
 
-  DiffractionModel.getPlainArray = matrix => {
-    const x = [];
-    for ( let i = 0; i < matrix.size; i++ ) {
-      x[ i ] = matrix.entries[ i ];
-    }
-    return x;
-  };
-
-  DiffractionModel.getFloat32Array = matrix => {
-    const x = new Float32Array( matrix.getColumnDimension() * matrix.getRowDimension() );
-    for ( let i = 0; i < matrix.size; i++ ) {
-      x[ i ] = matrix.entries[ i ];
-    }
-    return x;
-  };
-
   /**
    * Given row/column indices, find the index in the flattened array.  Uses the same strategy as DOT/Matrix.
    * @param {number} i - row index
@@ -153,14 +138,7 @@ define( require => {
       im[ i ] = 0;
     }
 
-    // TODO: supply input.entries as re?
     FFT.fft2d( re, im );
-
-    // TODO: inline this or don't allocate.  Also, should we be creating Complex all the time?
-    const result = [];
-    for ( let i = 0; i < re.length; i++ ) {
-      result[ i ] = new Complex( re[ i ], im[ i ] );
-    }
 
     // From https://www.cs.cmu.edu/afs/andrew/scs/cs/15-463/2001/pub/www/notes/fourier/fourier.pdf
     // Practical issues: For display purposes, you probably want to cyclically translate the picture so that pixel (0,0), which now contains frequency (ωx, ωy ) = (0, 0), moves to the center
@@ -169,28 +147,28 @@ define( require => {
 
     for ( let row = 0; row < MATRIX_DIMENSION; row++ ) {
       for ( let col = 0; col < MATRIX_DIMENSION; col++ ) {
-        const sourceIndex = getIndex( row, col );
-        const destinationIndex = getIndex(
+        const source = getIndex( row, col );
+        const destination = getIndex(
           ( row + MATRIX_DIMENSION / 2 ) % MATRIX_DIMENSION,
           ( col + MATRIX_DIMENSION / 2 ) % MATRIX_DIMENSION
         );
-        SCRATCH_SHIFTED[ destinationIndex ] = result[ sourceIndex ];
+        SHIFTED_MAGNITUDES[ destination ] = Math.sqrt( re[ source ] * re[ source + im[ source ] * im[ source ] ] );
       }
     }
 
     // get the largest magnitude
     let maxMagnitude = 0;
-    for ( let i = 0; i < SCRATCH_SHIFTED.length; i++ ) {
-      if ( SCRATCH_SHIFTED[ i ].magnitude > maxMagnitude ) {
-        maxMagnitude = SCRATCH_SHIFTED[ i ].magnitude;
+    for ( let i = 0; i < SHIFTED_MAGNITUDES.length; i++ ) {
+      if ( SHIFTED_MAGNITUDES[ i ] > maxMagnitude ) {
+        maxMagnitude = SHIFTED_MAGNITUDES[ i ];
       }
     }
 
     // draw the pixels
     const logOfMaxMag = Math.log( CONTRAST * maxMagnitude + 1 );
 
-    for ( let i = 0; i < SCRATCH_SHIFTED.length; i++ ) {
-      output.entries[ i ] = Math.log( CONTRAST * SCRATCH_SHIFTED[ i ].magnitude + 1 ) / logOfMaxMag;
+    for ( let i = 0; i < SHIFTED_MAGNITUDES.length; i++ ) {
+      output.entries[ i ] = Math.log( CONTRAST * SHIFTED_MAGNITUDES[ i ] + 1 ) / logOfMaxMag;
     }
   };
 
