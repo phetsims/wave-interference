7432dbf4386aff15b3f4dcf1523a0d969c429f83
samreid <reids@colorado.edu>
2019-03-11 09:16:27 -0600

    Factor out mapOutputViaContrast

--- js/diffraction/model/DiffractionModel.js
+++ js/diffraction/model/DiffractionModel.js
@@ -92,7 +92,7 @@ define( require => {
         // console.log(m);
 
         const y = Date.now();
-        DiffractionModel.fftImageProcessingLabs( this.scaledApertureMatrix, this.diffractionMatrix );
+        // DiffractionModel.fftImageProcessingLabs( this.scaledApertureMatrix, this.diffractionMatrix );
         const n = Date.now() - y;
         // console.log(n);
 
@@ -138,6 +138,31 @@ define( require => {
   };
 
   /**
+   * @param {Complex[]} result - 1d array representing matrix
+   * @param {Matrix} output - 1d array for outputs
+   */
+  DiffractionModel.mapOutputViaContrast = ( result, output ) => {
+
+    // Rearrange quadrants
+    const shifted = Fourier.shift( result, MATRIX_DIMENSIONS );
+
+    // get the largest magnitude
+    let maxMagnitude = 0;
+    for ( let i = 0; i < shifted.length; i++ ) {
+      if ( shifted[ i ].magnitude > maxMagnitude ) {
+        maxMagnitude = shifted[ i ].magnitude;
+      }
+    }
+
+    // draw the pixels
+    const logOfMaxMag = Math.log( CONTRAST * maxMagnitude + 1 );
+
+    for ( let i = 0; i < shifted.length; i++ ) {
+      output.entries[ i ] = Math.log( CONTRAST * shifted[ i ].magnitude + 1 ) / logOfMaxMag;
+    }
+  };
+
+  /**
    * @param {Matrix} input
    * @param {Matrix} output
    */
@@ -145,24 +170,16 @@ define( require => {
     // compute the h hat values
     const result = [];
     Fourier.transform( DiffractionModel.getPlainArray( input ), result );
-    const shifted = Fourier.shift( result, MATRIX_DIMENSIONS );
+    const complexes = result.map( c => new Complex( c.real, c.imag ) );
 
-    // get the largest magnitude
-    let maxMagnitude = 0;
-    for ( let i = 0; i < shifted.length; i++ ) {
-      if ( shifted[ i ].magnitude() > maxMagnitude ) {
-        maxMagnitude = shifted[ i ].magnitude();
-      }
-    }
-
-    // draw the pixels
-    const logOfMaxMag = Math.log( CONTRAST * maxMagnitude + 1 );
-
-    for ( let i = 0; i < result.length; i++ ) {
-      output.entries[ i ] = Math.log( CONTRAST * shifted[ i ].magnitude() + 1 ) / logOfMaxMag;
-    }
+    // Patch to reuse output code
+    DiffractionModel.mapOutputViaContrast( complexes, output );
   };
 
+  // From https://www.cs.cmu.edu/afs/andrew/scs/cs/15-463/2001/pub/www/notes/fourier/fourier.pdf
+  // Practical issues: For display purposes, you probably want to cyclically translate the picture so that pixel (0,0), which now contains frequency (ωx, ωy ) = (0, 0), moves to the center
+  // of the image. And you probably want to display pixel values proportional to log(magnitude)
+  // of each complex number (this looks more interesting than just magnitude). For color images, do the above to each of the three channels (R, G, and B) independently.
   DiffractionModel.fftImageProcessingLabs = ( input, output ) => {
     FFT.init( input.getRowDimension() );
     const im = new Array( input.entries.length );
@@ -175,34 +192,12 @@ define( require => {
 
     // FrequencyFilter.swap( re, im );
 
-    // From https://www.cs.cmu.edu/afs/andrew/scs/cs/15-463/2001/pub/www/notes/fourier/fourier.pdf
-    // Practical issues: For display purposes, you probably want to cyclically translate the picture so that pixel (0,0), which now contains frequency (ωx, ωy ) = (0, 0), moves to the center
-    // of the image. And you probably want to display pixel values proportional to log(magnitude)
-    // of each complex number (this looks more interesting than just magnitude). For color images, do the above to each of the three channels (R, G, and B) independently.
-
     const result = [];
     for ( let i = 0; i < re.length; i++ ) {
       result[ i ] = new Complex( re[ i ], im[ i ] );
     }
 
-    // Rearrange quadrants
-    const shifted = Fourier.shift( result, MATRIX_DIMENSIONS );
-
-    // get the largest magnitude
-    let maxMagnitude = 0;
-    for ( let i = 0; i < shifted.length; i++ ) {
-      if ( shifted[ i ].magnitude > maxMagnitude ) {
-        maxMagnitude = shifted[ i ].magnitude;
-      }
-    }
-    // const maxMagnitude = Math.max( ...shifted.map( h => h.magnitude ) );
-
-    // draw the pixels
-    const logOfMaxMag = Math.log( CONTRAST * maxMagnitude + 1 );
-
-    for ( let i = 0; i < shifted.length; i++ ) {
-      output.entries[ i ] = Math.log( CONTRAST * shifted[ i ].magnitude + 1 ) / logOfMaxMag;
-    }
+    DiffractionModel.mapOutputViaContrast( result, output );
   };
 
   return waveInterference.register( 'DiffractionModel', DiffractionModel );
