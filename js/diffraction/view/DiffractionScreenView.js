ebea375e3b88646567634e433b8d4092b018a348
samreid <reids@colorado.edu>
2019-04-23 10:58:13 -0600

    Remove unused query parameter

--- js/diffraction/view/DiffractionScreenView.js
+++ js/diffraction/view/DiffractionScreenView.js
@@ -34,7 +34,6 @@ define( require => {
   const VisibleColor = require( 'SCENERY_PHET/VisibleColor' );
   const waveInterference = require( 'WAVE_INTERFERENCE/waveInterference' );
   const WaveInterferenceConstants = require( 'WAVE_INTERFERENCE/common/WaveInterferenceConstants' );
-  const WaveInterferenceQueryParameters = require( 'WAVE_INTERFERENCE/common/WaveInterferenceQueryParameters' );
   const WaveInterferenceText = require( 'WAVE_INTERFERENCE/common/view/WaveInterferenceText' );
   const WavelengthSlider = require( 'SCENERY_PHET/WavelengthSlider' );
   const WavingGirlSceneControlPanel = require( 'WAVE_INTERFERENCE/diffraction/view/WavingGirlSceneControlPanel' );
