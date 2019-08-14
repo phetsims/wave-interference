653a7106b151fc0c1a13b4805f07df5e3996464a
samreid <reids@colorado.edu>
2019-08-14 13:34:43 -0600

    Reduce line length, see https://github.com/phetsims/wave-interference/issues/426

--- js/waves/view/WavesScreenSoundView.js
+++ js/waves/view/WavesScreenSoundView.js
@@ -40,13 +40,15 @@ define( require => {
 
       // The sound scene generates a sine wave when the "Play Tone" checkbox is checked
       if ( model.soundScene && options.controlPanelOptions.showPlaySoundControl ) {
-        const sineWavePlayer = new SineWaveGenerator( model.soundScene.frequencyProperty, model.soundScene.amplitudeProperty, {
-          enableControlProperties: [
-            model.soundScene.isSoundPlayingProperty,
-            model.soundScene.button1PressedProperty,
-            model.isRunningProperty
-          ]
-        } );
+        const sineWavePlayer = new SineWaveGenerator(
+          model.soundScene.frequencyProperty,
+          model.soundScene.amplitudeProperty, {
+            enableControlProperties: [
+              model.soundScene.isSoundPlayingProperty,
+              model.soundScene.button1PressedProperty,
+              model.isRunningProperty
+            ]
+          } );
 
         // Suppress the tone when another screen is selected
         soundManager.addSoundGenerator( sineWavePlayer, {
@@ -56,9 +58,9 @@ define( require => {
         sineWavePlayer.addEnableControlProperty( isSoundSceneProperty );
       }
 
-      // Additional sounds which are enabled in ?fullSonification.  Note once we publish Wave Interference and Waves Intro
-      // with just the sine wave (above), we plan to republish the entire sim with a11y and full sonification.  At that
-      // point, the ?fullSonification query parameter will be removed.
+      // Additional sounds which are enabled in ?fullSonification.  Note once we publish Wave Interference and Waves
+      // Intro with just the sine wave (above), we plan to republish the entire sim with a11y and full sonification.
+      // At that point, the ?fullSonification query parameter will be removed.
       // TODO: Remove this once we publish with full sonification
       if ( WaveInterferenceQueryParameters.fullSonification ) {
         soundManager.addSoundGenerator( new ResetAllSoundGenerator( model.isResettingProperty, {
@@ -88,11 +90,14 @@ define( require => {
 
             // The waterDrop.amplitude indicates the size of the water drop and the strength of the resulting wave.
             // Smaller water drops play with a higher frequency.
-            const amplitude = Util.linear( WaveInterferenceConstants.AMPLITUDE_RANGE.min, WaveInterferenceConstants.AMPLITUDE_RANGE.max,
-              1.0, 0.5, waterDrop.amplitude );
+            const amplitude = Util.linear(
+              WaveInterferenceConstants.AMPLITUDE_RANGE.min, WaveInterferenceConstants.AMPLITUDE_RANGE.max,
+              1.0, 0.5, waterDrop.amplitude
+            );
 
             // Select water drop sounds randomly, but do not let the same sound go twice in a row
-            lastPlayedWaterDropSoundClip = phet.joist.random.sample( _.without( soundClips, lastPlayedWaterDropSoundClip ) );
+            const availableClips = _.without( soundClips, lastPlayedWaterDropSoundClip );
+            lastPlayedWaterDropSoundClip = phet.joist.random.sample( availableClips );
             lastPlayedWaterDropSoundClip.setPlaybackRate( amplitude );
             lastPlayedWaterDropSoundClip.play();
           } );
@@ -112,10 +117,14 @@ define( require => {
           // TODO (for the reviewer): is that last constraint about having to wait for the next cycle to hear change OK?
           model.soundScene.oscillator1Property.link( ( value, previousValue ) => {
             if ( previousValue >= 0 && value < 0 ) {
-              const amplitude = Util.linear( model.soundScene.amplitudeProperty.range.min, model.soundScene.amplitudeProperty.range.max,
-                0.0, 0.4, model.soundScene.amplitudeProperty.value );
-              const playbackRate = Util.linear( model.soundScene.frequencyProperty.range.min, model.soundScene.frequencyProperty.range.max,
-                1, 1.4, model.soundScene.frequencyProperty.value );
+              const amplitude = Util.linear(
+                model.soundScene.amplitudeProperty.range.min, model.soundScene.amplitudeProperty.range.max,
+                0.0, 0.4, model.soundScene.amplitudeProperty.value
+              );
+              const playbackRate = Util.linear(
+                model.soundScene.frequencyProperty.range.min, model.soundScene.frequencyProperty.range.max,
+                1, 1.4, model.soundScene.frequencyProperty.value
+              );
               speakerPulseSoundClip.setOutputLevel( amplitude, 0.5 );
               speakerPulseSoundClip.setPlaybackRate( playbackRate / 2 );
               speakerPulseSoundClip.play();
@@ -129,8 +138,8 @@ define( require => {
             loop: true
           } );
 
-          // TODO: @jbphet: the following line cuts the audio by about half when used instead of using the multilink below
-          // TODO: @jbphet: also note when I added associatedViewNode: view this also cut the volume approximately in half
+          // TODO: @jbphet: the following line cuts the audio by about half when used instead of using the multilink 
+          // TODO: below also note when I added associatedViewNode: view this also cut the volume approximately in half
           // lightBeamLoopSoundClip.addEnableControlProperty( model.lightScene.soundEffectEnabledProperty );
           soundManager.addSoundGenerator( lightBeamLoopSoundClip, {
             associatedViewNode: view
