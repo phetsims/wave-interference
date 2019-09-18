975c974e57640a9a53ffca010bb865a5734984bf
phet-dev <phetadmin@gmail.com>
2019-09-18 03:27:44 -0600

    update copyright dates from daily grunt work

--- js/common/view/ToolboxPanel.js
+++ js/common/view/ToolboxPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  * Shows the toolbox from whence tools (measuring tape, timer, probe) can be dragged.
