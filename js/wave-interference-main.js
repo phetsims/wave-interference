d34f4194e6471bf037d2d2ae57361939ce9dc175
samreid <reids@colorado.edu>
2019-09-23 08:52:57 -0600

    Added Logan Bray to credits, see https://github.com/phetsims/waves-intro/issues/9

--- js/wave-interference-main.js
+++ js/wave-interference-main.js
@@ -26,7 +26,7 @@ define( require => {
       leadDesign: 'Amy Rouinfar, Noah Podolefsky',
       softwareDevelopment: 'Sam Reid',
       team: 'Wendy Adams, Diana L\u00f3pez Tavares, Ariel Paul, Kathy Perkins, Katie Woessner',
-      qualityAssurance: 'Steele Dalton, Megan Lai, Liam Mulhall, Laura Rea, Jacob Romero, Katie Woessner, Kelly Wurtz',
+      qualityAssurance: 'Logan Bray, Steele Dalton, Megan Lai, Liam Mulhall, Laura Rea, Jacob Romero, Katie Woessner, Kelly Wurtz',
       graphicArts: 'Cheryl McCutchan',
       thanks: 'This material is based upon work supported by the National Science Foundation under STROBE Grant No. DMR ' +
               '1548924. Any opinions, findings, and conclusions or recommendations expressed in this material are those ' +
