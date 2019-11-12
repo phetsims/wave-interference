3d824df08f6637a6d2ebfa3cb1eca94d397635f7
jbphet <john.blanco@colorado.edu>
2019-11-12 08:36:53 -0700

    moved the flags for turning on sound from Sim.js options to package.json, see https://github.com/phetsims/chipper/issues/806

--- js/wave-interference-main.js
+++ js/wave-interference-main.js
@@ -32,8 +32,7 @@ define( require => {
               '1548924. Any opinions, findings, and conclusions or recommendations expressed in this material are those ' +
               'of the author(s) and do not necessarily reflect the views of the National Science Foundation.'
     },
-    webgl: platform.mobileSafari,
-    supportsSound: true
+    webgl: platform.mobileSafari
   };
 
   SimLauncher.launch( () => {
