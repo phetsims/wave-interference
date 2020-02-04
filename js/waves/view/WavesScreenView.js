a8a236d1f5425aac151f967f02e2db4f56ed738a
phet-dev <phetadmin@gmail.com>
2020-02-04 04:10:22 -0700

    update copyright dates from daily grunt work

--- js/waves/view/WavesScreenView.js
+++ js/waves/view/WavesScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2019, University of Colorado Boulder
+// Copyright 2018-2020, University of Colorado Boulder
 
 /**
  * View for the "Waves" screen.  Extended for the Interference and Slits screens.
