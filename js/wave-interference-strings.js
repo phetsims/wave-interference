4acec43332eb876bb24de5fe27e07ab3906c5c11
samreid <reids@colorado.edu>
2020-02-27 12:07:59 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/wave-interference-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import waveInterference from './waveInterference.js';
+
+const waveInterferenceStrings = getStringModule( 'WAVE_INTERFERENCE' );
+
+waveInterference.register( 'waveInterferenceStrings', waveInterferenceStrings );
+
+export default waveInterferenceStrings;
