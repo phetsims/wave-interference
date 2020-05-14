d63683b6fdeb03ad2ed4555c696d4784721c1673
samreid <reids@colorado.edu>
2020-05-14 14:34:51 -0600

    Decrease membrane volume to 0.6, see https://github.com/phetsims/wave-interference/issues/473

--- js/waves/view/WavesScreenSoundView.js
+++ js/waves/view/WavesScreenSoundView.js
@@ -116,7 +116,7 @@ class WavesScreenSoundView {
         view.waveMeterNode.duckingProperty
       ], ( oscillatorValue, isTonePlaying, ducking ) => {
         if ( previousOscillatorValue >= 0 && oscillatorValue < 0 ) {
-          const maxVolume = isTonePlaying ? 0.266 : 0.5;
+          const maxVolume = ( isTonePlaying ? 0.266 : 0.5 ) * 0.6;
           const outputLevel = Utils.linear(
             // The tone takes precedence over the membrane sound, another level of ducking
             model.soundScene.amplitudeProperty.range.min, model.soundScene.amplitudeProperty.range.max,
