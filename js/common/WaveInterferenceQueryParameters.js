5f624055928f9fa5ee2cfc899aa05ccbbe6c957d
samreid <reids@colorado.edu>
2021-01-21 18:18:59 -0700

    Add public: true to query parameters, see https://github.com/phetsims/tasks/issues/1076

--- js/common/WaveInterferenceQueryParameters.js
+++ js/common/WaveInterferenceQueryParameters.js
@@ -13,7 +13,7 @@ const WaveInterferenceQueryParameters = QueryStringMachine.getAll( {
   // Shows the overlays for the theoretical/ideal (far-field) interference patterns on the "Slits" screen. Starting in
   // 1.1+, this will be shared with teachers and should not be changed lightly.
   // See https://github.com/phetsims/wave-interference/issues/196
-  theory: { type: 'flag' },
+  theory: { type: 'flag', public: true },
 
   latticeSize: { type: 'number', defaultValue: 151 }
 } );
