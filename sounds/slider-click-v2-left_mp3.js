28fe06ce9ab2acd95d98225d7e05ce271f16fe65
zepumph <michael.kauzmann@colorado.edu>
2021-01-22 13:10:02 -0900

    use asyncLoader instead of simLauncher, plus grunt update on accident, https://github.com/phetsims/joist/issues/687

--- sounds/slider-click-v2-left_mp3.js
+++ sounds/slider-click-v2-left_mp3.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
-import simLauncher from '../../joist/js/simLauncher.js';
+import asyncLoader from '../../phet-core/js/asyncLoader.js';
 import base64SoundToByteArray from '../../tambo/js/base64SoundToByteArray.js';
 import WrappedAudioBuffer from '../../tambo/js/WrappedAudioBuffer.js';
 import phetAudioContext from '../../tambo/js/phetAudioContext.js';
 
 const soundURI = 'data:audio/mpeg;base64,//swxAAAB4Q7OnWEgBEOj+0rMoACAJKY4LTo/xZKgCAMhDIAvhDKEhFBplWG4fl+UoBMNk+/3OaBgMQQBMP/B8P4IO+UdwQOf+sHAQBA5//3AAVKefuCUCgAAAACMM2USoCbVzORGiXrcx4qdqHvO1ilu/sLlm/gHAXgAUof/IeOPGix1z+p6RJqpt/9nGBcuOEv9yUAARklIk+D//syxAOCCGRVLV3EABC0A2MpjSRKA42M0mQOPxk5HmgkEdJexpMfBcKmKhiYZByjK7X558phl2QVA2O/KOFhVf/1/aRUFTol/Xo/7f/R/R/1AWw20lgmSZBlsRBg6uoXDGSAqZDZsSolQVKnSKh1bpL/lXeVd//Wd/5Z87/0qnXYrGAABAAwg7wMATwZUIUCZCTASQupAAFsGgL88P/7MsQPA8U4Jr2HpeIgAAA0gAAABFWyU///////+LCwjkxBTUUzLjk5LjWqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqo=';
 const soundByteArray = base64SoundToByteArray( phetAudioContext, soundURI );
-const unlock = simLauncher.createLock( soundURI );
+const unlock = asyncLoader.createLock( soundURI );
 const wrappedAudioBuffer = new WrappedAudioBuffer();
 const onDecodeSuccess = decodedAudio => {
   wrappedAudioBuffer.audioBufferProperty.set( decodedAudio );
--- sounds/slider-click-v2-right_mp3.js
+++ sounds/slider-click-v2-right_mp3.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
-import simLauncher from '../../joist/js/simLauncher.js';
+import asyncLoader from '../../phet-core/js/asyncLoader.js';
 import base64SoundToByteArray from '../../tambo/js/base64SoundToByteArray.js';
 import WrappedAudioBuffer from '../../tambo/js/WrappedAudioBuffer.js';
 import phetAudioContext from '../../tambo/js/phetAudioContext.js';
 
 const soundURI = 'data:audio/mpeg;base64,//swxAAABkgZTTSRgDE4FC53MJADAAAoaagJ21AoCAIEjlxQKEByCAPn36z+AAQBCXB8P9YPwICAY/+CDv0A+D4Pg+BAQ/pIKTcibcFuu2w+AAAAASPNrXxMSnZL9hG1IF4qcOx7/5UrcatKJ9Oyrk5ltMsTf9AUAw61utV/1htDKajUfcb/ElJ69SVah+f//m8dXGmVAAr9pSIm//syxAMAB8w1N12zADB9gqd0ZhhOBkQKAjACk1aRPI8TtEcywlMaEzCAMu8nVAz/RqmpqbICciGJEiRIPnaPBrU+sNfWy7+j/5D+r1VghJKCjAWgMA00J0OQpC4zCIDz5bf7v/z3rO////loabf9d+AEpAP0QGiS4JRJHE4jEh9lrO0qTEFNRTMuOTkuNaqqqqqqqqqqqqqqqqqqqv/7MsQYA8LMIvCGMSK4AAA0gAAABKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqo=';
 const soundByteArray = base64SoundToByteArray( phetAudioContext, soundURI );
-const unlock = simLauncher.createLock( soundURI );
+const unlock = asyncLoader.createLock( soundURI );
 const wrappedAudioBuffer = new WrappedAudioBuffer();
 const onDecodeSuccess = decodedAudio => {
   wrappedAudioBuffer.audioBufferProperty.set( decodedAudio );
