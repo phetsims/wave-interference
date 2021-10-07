618f994a0598f38e321a0cc809c02a9e9eeb94f2
jbphet <john.blanco@colorado.edu>
2021-10-07 17:30:31 -0600

    re-modulified sound files - again - with better error handling, see https://github.com/phetsims/tambo/issues/151 and https://github.com/phetsims/tambo/issues/152

--- sounds/light-beam-loop-v5-eq-out-bass_mp3.js
+++ sounds/light-beam-loop-v5-eq-out-bass_mp3.js
@@ -8,15 +8,39 @@ const soundURI = 'data:audio/mpeg;base64,//uQxAAAAAAAAAAAAAAAAAAAAAAAWGluZwAAAA8
 const soundByteArray = base64SoundToByteArray( phetAudioContext, soundURI );
 const unlock = asyncLoader.createLock( soundURI );
 const wrappedAudioBuffer = new WrappedAudioBuffer();
+
+// safe way to unlock
+let unlocked = false;
+const safeUnlock = () => {
+  if ( !unlocked ) {
+    unlock();
+    unlocked = true;
+  }
+};
+
 const onDecodeSuccess = decodedAudio => {
-  wrappedAudioBuffer.audioBufferProperty.set( decodedAudio );
-  unlock();
+  if ( wrappedAudioBuffer.audioBufferProperty.value === null ) {
+    wrappedAudioBuffer.audioBufferProperty.set( decodedAudio );
+    safeUnlock();
+  }
 };
 const onDecodeError = decodeError => {
   console.warn( 'decode of audio data failed, using stubbed sound, error: ' + decodeError );
   wrappedAudioBuffer.audioBufferProperty.set( phetAudioContext.createBuffer( 1, 1, phetAudioContext.sampleRate ) );
-  unlock();
+  safeUnlock();
 };
-phetAudioContext.decodeAudioData( soundByteArray.buffer, onDecodeSuccess, onDecodeError )
-  .catch( e => { console.warn( 'promise rejection caught for audio decode, error = ' + e ) } );
+const decodePromise = phetAudioContext.decodeAudioData( soundByteArray.buffer, onDecodeSuccess, onDecodeError );
+if ( decodePromise ) {
+  decodePromise
+    .then( decodedAudio => {
+      if ( wrappedAudioBuffer.audioBufferProperty.value === null ) {
+        wrappedAudioBuffer.audioBufferProperty.set( decodedAudio );
+        safeUnlock();
+      }
+    } )
+    .catch( e => {
+      console.warn( 'promise rejection caught for audio decode, error = ' + e );
+      safeUnlock();
+    } );
+}
 export default wrappedAudioBuffer;
