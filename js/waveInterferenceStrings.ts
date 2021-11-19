f84bcb9c937fe9d50ae81723aedeb295184b8785
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:42 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/waveInterferenceStrings.ts
@@ -0,0 +1,85 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import waveInterference from './waveInterference.js';
+
+type StringsType = {
+  'wave-interference': {
+    'title': string
+  },
+  'screen': {
+    'waves': string,
+    'interference': string,
+    'slits': string,
+    'diffraction': string
+  },
+  'frequency': string,
+  'diameter': string,
+  'eccentricity': string,
+  'amplitude': string,
+  'graph': string,
+  'waterLevel': string,
+  'waterLevelAtCenter': string,
+  'pressure': string,
+  'electricField': string,
+  'electricFieldAtCenter': string,
+  'playTone': string,
+  'pressureAtCenter': string,
+  'positionCM': string,
+  'positionNM': string,
+  'cmValue': string,
+  'nmValue': string,
+  'mmValue': string,
+  'degreesValue': string,
+  'min': string,
+  'max': string,
+  'centimetersUnits': string,
+  'nanometersUnits': string,
+  'secondsUnits': string,
+  'millisecondsUnits': string,
+  'femtosecondsUnits': string,
+  'millisecondConversion': string,
+  'femtosecondConversion': string,
+  'screenLabel': string,
+  'intensity': string,
+  'topView': string,
+  'sideView': string,
+  'distanceUnits': string,
+  'timeUnits': string,
+  'normal': string,
+  'slow': string,
+  'top': string,
+  'separation': string,
+  'noBarrier': string,
+  'oneSlit': string,
+  'twoSlits': string,
+  'time': string,
+  'slitWidth': string,
+  'width': string,
+  'height': string,
+  'circleDiameter': string,
+  'rotation': string,
+  'squareWidth': string,
+  'latticeSpacing': string,
+  'disorder': string,
+  'slitSeparation': string,
+  'waves': string,
+  'particles': string,
+  'both': string,
+  'waterWaveGenerator': string,
+  'soundGenerator': string,
+  'lightGenerator': string,
+  'soundEffect': string,
+  'none': string,
+  'lots': string
+};
+
+const waveInterferenceStrings = getStringModule( 'WAVE_INTERFERENCE' ) as StringsType;
+
+waveInterference.register( 'waveInterferenceStrings', waveInterferenceStrings );
+
+export default waveInterferenceStrings;
