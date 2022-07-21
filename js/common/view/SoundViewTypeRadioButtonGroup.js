f23722a40641ce5f32e9360ba3045685a1b36494
phet-dev <phetadmin@gmail.com>
2022-07-21 05:50:18 -0600

    update copyright dates from daily grunt work

--- js/common/view/SoundViewTypeRadioButtonGroup.js
+++ js/common/view/SoundViewTypeRadioButtonGroup.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2020, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * Convenience class for the radio button group that chooses between SoundScene.SoundViewType.VALUES.
--- js/slits/SlitsScreen.js
+++ js/slits/SlitsScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2022, University of Colorado Boulder
 
 /**
  * "Slits" screen in the Wave Interference simulation.
