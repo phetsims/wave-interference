5fa0549b7f4b6c315ff5a41323faa113d5e330a7
Sam Reid <reids@colorado.edu>
2022-08-28 19:25:24 -0600

    Add type checking, see https://github.com/phetsims/wave-interference/issues/529

--- js/common/model/WaterDrop.ts
+++ js/common/model/WaterDrop.ts
@@ -1,5 +1,5 @@
 // Copyright 2018-2020, University of Colorado Boulder
-// @ts-nocheck
+
 /**
  * Model for a single water drop, which conveys a new value for amplitude and frequency to the model.
  * For water, the controls set desiredAmplitude, desiredFrequency.  Those values are transmitted by a water drop.
@@ -17,38 +17,26 @@ const INITIAL_DISTANCE_ABOVE_LATTICE = 100; // in view coordinates
 
 class WaterDrop {
 
+  public y = INITIAL_DISTANCE_ABOVE_LATTICE;
+
+  // In side view, if the drop has gone beneath the water, it gets absorbed.  In this case, it means it
+  // should no longer be visible.  But the modeled time that it affects the lattice is the same.
+  public absorbed = false;
+
   /**
    * @param amplitude - strength of the wave
    * @param startsOscillation - false if this is a fake water drop to shut off the oscillation at one cycle
-   * @param sourceSeparation - the vertical coordinate of the cell that the water drop is falling to
+   * @param sourceSeparation - the vertical coordinate of the cell that the water drop is falling to.
+   *                 - the distance between the sources when this drop was released, used to show the correct position
+   *                 - of the water drop
    * @param sign - -1 for top faucet, +1 for bottom faucet
    * @param onAbsorption - called when the water drop is absorbed by the lattice
    */
-  public constructor( amplitude: number, startsOscillation: boolean, sourceSeparation: number, sign: number, onAbsorption: () => void ) {
-
-    // @public (read-only)
-    this.amplitude = amplitude;
-
-    // @public (read-only)
-    this.startsOscillation = startsOscillation;
-
-    // @public (read-only)
-    this.y = INITIAL_DISTANCE_ABOVE_LATTICE;
-
-    // @public - In side view, if the drop has gone beneath the water, it gets absorbed.  In this case, it means it
-    // should no longer be visible.  But the modeled time that it affects the lattice is the same.
-    this.absorbed = false;
-
-    // @public (read-only) {function} - called when absorbed
-    this.onAbsorption = onAbsorption;
-
-    // @public (read-only) - the distance between the sources when this drop was released, used to show the
-    // correct position of the water drop
-    this.sourceSeparation = sourceSeparation;
-
-    // @public (read-only) {string}
-    this.sign = sign;
-  }
+  public constructor( public readonly amplitude: number,
+                      public readonly startsOscillation: boolean,
+                      public readonly sourceSeparation: number,
+                      public readonly sign: number,
+                      public readonly onAbsorption: () => void ) {}
 
   /**
    * Animate the water drop at a constant velocity toward the point at which it hits the water surface.
